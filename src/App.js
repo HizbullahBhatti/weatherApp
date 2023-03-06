c56e9492f35faebce078860fcea424c416c7b34a
HizbullahBhatti <hizbullah.kcsf18@iba-suk.edu.pk>
2023-03-06 21:29:44 +0500

    Weather card done now API Call

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-
+import Temp from './components/temp';
 
 function App() {
   return (
-    <><h1>Hello</h1></>
+    <>
+    <Temp />
+    
+    
+    </>
+
   );
 }
 
---
+++ src/components/temp.js
@@ -0,0 +1,105 @@
+import React, { useEffect, useState } from 'react'
+import "./style.css"
+
+const Temp = () => {
+    const[searchValue, setSearchValue] = useState('karachi')
+
+    const getWeatherInfo = async () => {
+        try {
+            let url = `https://api.openweathermap.org/data/2.5/weather?q=karachi&appid=0fcfcc5cf5682e7555b9d533e48365bd`
+
+            const res = await fetch(url)
+            const data = await res.json()
+            console.log(data)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        getWeatherInfo()
+    },[])
+
+  return (
+    <>
+    <div className="wrap">
+        <div className="search">
+            <input type="search" 
+            placeholder='search ...'
+            autoFocus
+            id='search'
+            value={searchValue}
+            onChange={(e)=>{setSearchValue(e.target.value)}}
+            className='searchTerm'/>
+
+            <button className='searchButton' type='button' onClick={getWeatherInfo}>Searh</button>    
+        </div>
+    </div>
+
+    <article className="widget">
+        <div className="weatherIcon">
+            <i className="wi wi-day-sunny"></i>
+        </div>
+        <div className="weatherInfo">
+            <div className="temperature">
+                <span>25.5&deg;</span>
+            </div>
+            <div className="description">
+                <div className="weatherCondition">Sunny</div>
+                <div className="place">London, UK</div>
+            </div>
+        </div>
+        <div className="date">{new Date().toLocaleString()}</div>
+        
+        <div className="extra-temp">
+            <div className="temp-info-minmax">
+                <div className="two-sided-section">
+                    <p>
+                        <i className="wi wi-sunset"></i>
+                    </p>
+                    <p className="extra-info-leftside">
+                        18:00 PM<br/>
+                        Sunset
+                    </p>
+                </div>
+
+                <div className="two-sided-section">
+                    <p>
+                        <i className="wi wi-humidity"></i>
+                    </p>
+                    <p className="extra-info-leftside">
+                        18:00 PM<br/>
+                        Humidity
+                    </p>
+                </div>
+
+            </div>
+            <div className="weather-extra-info">
+            <div className="two-sided-section">
+                    <p>
+                        <i className="wi wi-rain"></i>
+                    </p>
+                    <p className="extra-info-leftside">
+                        18:00 PM<br/>
+                        Pressure
+                    </p>
+                </div>
+                <div className="two-sided-section">
+                    <p>
+                        <i className="wi wi-strong-wind"></i>
+                    </p>
+                    <p className="extra-info-leftside">
+                        18:00 PM<br/>
+                        Humidity
+                    </p>
+                </div>
+            </div>
+        </div>
+    </article>
+    
+    
+    </>
+  )
+}
+
+export default Temp
